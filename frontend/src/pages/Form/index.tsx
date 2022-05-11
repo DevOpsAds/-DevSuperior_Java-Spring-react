48a70f5160f24781a82cf6d0e2f2c668892f5712
DevOpsAds <joaobatistalimajunior.ads@gmail.com>
2022-05-11 18:25:04 -0300

    Security config

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Pagination } from "../../components/Pagination";
 
 import "./styles.css"
@@ -37,8 +38,12 @@ export function Form() {
           <div className="dsmovie-form-btn-container">
             <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
           </div>
+          
         </form >
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        <Link to="/">
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+
+        </Link>
       </div >
     </div >
 
